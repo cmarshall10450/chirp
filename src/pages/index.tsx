21c79ff0b8d8208748dbfdc85d033eee3046876c
Chris Marshall <chris.marshall@optimapartners.co.uk>
2023-04-23 13:59:21 +0100

    Add posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
-
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -23,6 +21,11 @@ const Home: NextPage = () => {
           {!!user.isSignedIn && <SignOutButton />}
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
